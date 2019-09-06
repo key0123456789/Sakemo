aad64295c9aa7dace26f6c3e152150af960556eb
key0123456789 <k.i-0123456789@live.jp>
2019-09-07 00:49:46 +0900

    add-show-action

--- app/controllers/brands_controller.rb
+++ app/controllers/brands_controller.rb
@@ -1,10 +1,15 @@
 class BrandsController < ApplicationController
+
   def index
     @brands = Brand.search(params[:search])
   end
 
   def search
     @brands = Brand.search(params[:search])
+  end
+
+  def show
+    
   end
 
 end
