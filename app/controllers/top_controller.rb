925625afd17e3b1190a7c4a13892b2e17a963881
key0123456789 <k.i-0123456789@live.jp>
2019-12-20 01:56:46 +0900

    Create top page

--- app/controllers/top_controller.rb
+++ app/controllers/top_controller.rb
@@ -1,6 +1,16 @@
 class TopController < ApplicationController
 
   def index
+    @brands = Brand.search(params[:search])
   end
   
+  def search
+    return nil if params[:keyword] == ""
+    @brands = Brand.where(['name LIKE ?', "%#{params[:keyword]}%"] )
+    respond_to do |format|
+      format.html
+      format.json
+    end
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,14 @@ Rails.application.routes.draw do
   devise_for :users
 
   resources :users, only: :show
+  
   resources :brands, only: [:index, :show] do
+    collection do
+      get 'search'
+    end
+  end
+
+  resources :top, only: [:index] do
     collection do
       get 'search'
     end
