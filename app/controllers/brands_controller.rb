cd312ef3e9e95c90a57f44d22a16528963f0266f
key0123456789 <k.i-0123456789@live.jp>
2019-12-21 15:48:58 +0900

    Remove some dead code

--- app/controllers/brands_controller.rb
+++ app/controllers/brands_controller.rb
@@ -4,16 +4,7 @@ class BrandsController < ApplicationController
     @brands = Brand.search(params[:search])
   end
 
-  def search
-    @brands = Brand.search(params[:search]).limit(10)
-    respond_to do |format|
-      format.html
-      format.json
-    end
-  end
-
   def show
-    
   end
 
 end
