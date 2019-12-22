639ddfce4739b85ad8f2a7fd8727bb5bca214959
key0123456789 <k.i-0123456789@live.jp>
2019-12-22 22:28:06 +0900

    Search from multi columns

--- app/controllers/top_controller.rb
+++ app/controllers/top_controller.rb
@@ -7,7 +7,7 @@ class TopController < ApplicationController
   def search
     keyword = "%#{params[:keyword]}%"
     return nil if params[:keyword] == ""
-    @brands = Brand.find_by_sql(['select * from  brands where name LIKE ? LIMIT 20', keyword])
+    @brands = Brand.find_by_sql(['select * from brands where name LIKE ? OR name_kana LIKE ? LIMIT 20', keyword, keyword])
     respond_to do |format|
       format.html
       format.json
