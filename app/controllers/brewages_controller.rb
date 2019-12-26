9da6e377bd104e0a62249fb5c4ef35103594768e
key0123456789 <k.i-0123456789@live.jp>
2019-12-27 01:19:57 +0900

    Create temporary routes

--- app/controllers/brewages_controller.rb
+++ app/controllers/brewages_controller.rb
@@ -6,11 +6,20 @@ class BrewagesController < ApplicationController
   end
 
   def new
-
+    redirect_to login_url unless user_signed_in?
+    @brand = Brand.find(params[:brand_id])
+    @brewage = Brewage.new
   end
-
+  
   def create
-
+    @brand = Brand.find(params[:brand_id])
+    @brewage = Brewage.create(brewage_params)
+    redirect_to brand_brewage_path(@brewage, @brand)
   end
 
+  private
+
+  def brewage_params
+    params.require(:brewage).permit(:id, :name, :image).merge(brand_id: params[:brand_id])
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,22 @@
 Rails.application.routes.draw do
   devise_for :users
+  # ,
+  #  controllers: {
+  #   registrations: 'users/registrations',
+  #   sessions: 'users/sessions'
+  # }
 
+  # devise_scope :user do
+  #   get 'login',      to: 'users/sessions#new'
+  #   post 'login',     to: 'users/sessions#create'
+  #   get 'logout',     to: 'users/sessions#logout'
+  #   delete 'logout',  to: 'users/sessions#destroy'
+  #   get 'signup',     to: 'users/registrations#signup'
+  # end
   resources :users, only: :show
   
   resources :brands, only: [:index, :show] do
-    resources :brewages
+    resources :brewages, only: [:show, :new, :create]
   end
 
   resources :top, only: [:index] do
