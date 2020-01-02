cb7ee62587eff05a11a0b274eb7d6dcb56d0cba2
key0123456789 <k.i-0123456789@live.jp>
2020-01-03 02:14:37 +0900

    Update user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,4 +9,8 @@ class User < ApplicationRecord
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, uniqueness: true, format: { with: VALID_EMAIL_REGEX }
   validates :nickname, presence: true
+
+  def remember_me
+    true
+  end
 end
