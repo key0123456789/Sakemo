11a06fd79b9697d08ba4cdd88dde2493f2bd3a07
key0123456789 <k.i-0123456789@live.jp>
2020-01-03 22:40:31 +0900

    Order feelings

--- app/controllers/brands_controller.rb
+++ app/controllers/brands_controller.rb
@@ -8,7 +8,7 @@ class BrandsController < ApplicationController
     @brand = Brand.find(params[:id])
     @brewages = @brand.brewages.where(params[:brand_id])
     @brand_id = Brand.find(params[:id]).id
-    @feelings = @brand.feelings.where(params[:brand_id])
+    @feelings = @brand.feelings.order(created_at: :DESC).where(params[:brand_id])
     if user_signed_in?
       @user_id = current_user.id
     end
