577a24f4d1f6f3627220b5cd0a0e0f40125b2465
key0123456789 <k.i-0123456789@live.jp>
2020-01-10 01:32:35 +0900

    Add new items

--- app/controllers/brands_controller.rb
+++ app/controllers/brands_controller.rb
@@ -10,7 +10,9 @@ class BrandsController < ApplicationController
     @brand_id = Brand.find(params[:id]).id
     @feelings = @brand.feelings.order(created_at: :DESC).where(params[:brand_id])
     if user_signed_in?
+      @user = User.find(current_user.id)
       @user_id = current_user.id
+      @user_feeling = Feeling.find_by(brand_id: @brand_id, user_id: current_user.id)
     end
   end
 
