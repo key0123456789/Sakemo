ad605bf0885b29fd27c38ff12214547f58aa6c6b
key0123456789 <k.i-0123456789@live.jp>
2020-01-12 17:50:04 +0900

    Edit slider

--- app/assets/javascripts/slider.js
+++ app/assets/javascripts/slider.js
@@ -1,17 +1,16 @@
 
-$(document).on('turbolinks:load', function(){
+jQuery(document).on('turbolinks:load', function(){
 
-  $(function() {
-    $('.slide-image').slick({
+  jQuery('.slide-image').slick({
         dots: true,
         autoplay: true,
         autoplaySpeed: 6000,
         speed: 800,
+        // variableWidth: true,
         // font-size: 14px !important;
         // color: rgb(60, 60, 60) !important;
         prevArrow:'<i class="fa fa-angle-left arrow arrow-left"></i>',
         nextArrow:'<i class="fa fa-angle-right arrow arrow-right"></i>'
     });
-  });
 
 });
