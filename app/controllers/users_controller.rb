febd584d3fd9e1cc3f0394104f1d45b575586280
key0123456789 <k.i-0123456789@live.jp>
2020-01-12 15:23:21 +0900

    Create users/show page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,8 @@
 class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
+    @clip_brands = current_user.brands.page(params[:page]).per(20)
+    @user_feelings = Feeling.where(user_id: current_user.id).includes(:brand)
+    @feeling_pages = @user_feelings.page(params[:page]).per(20)
   end
 end
